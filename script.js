d2dc699db5c7f6db99c2df9f6ab68dd1990d2393
fakhar98 <fakharkaramat9@gmail.com>
2025-05-20 20:24:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Aug 11, 2024 00:00:00").getTime();
+var countDownDate = new Date("July 11, 2025 00:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
